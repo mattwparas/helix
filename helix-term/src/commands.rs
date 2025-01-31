05d080196adc19358b496364feffbe52d8dc43d4
Michael Davis <mcarsondavis@gmail.com>
2025-01-31 14:07:22 -0500

    Set a statusline error for formatter errors in `:format`

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -3480,6 +3480,10 @@ async fn make_format_callback(
                 }
             }
             Err(err) => {
+                if write.is_none() {
+                    editor.set_error(err.to_string());
+                    return;
+                }
                 log::info!("failed to format '{}': {err}", doc.display_name());
             }
         }
